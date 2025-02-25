a688e7a13e0bd2e252c1dd28d8adf973a967f51a
Sahil Sanghvi <sahil.sanghvi@rutgers.edu>
2025-02-24 21:48:25 -0500

    Update adaptiveA.py

--- adaptiveA.py
+++ adaptiveA.py
@@ -8,13 +8,12 @@ def heuristic(a, b, h_values):
     return h_values.get(a, abs(a[0] - b[0]) + abs(a[1] - b[1]))
 
 def adaptive_astar(maze, start, goal, tie_breaking='smaller_g'):
-    
     def get_neighbors(x, y):
         neighbors = []
         if x > 0: neighbors.append((x - 1, y))
-        if x < maze.shape[1] - 1: neighbors.append((x + 1, y))
+        if x < maze.shape[0] - 1: neighbors.append((x + 1, y))
         if y > 0: neighbors.append((x, y - 1))
-        if y < maze.shape[0] - 1: neighbors.append((x, y + 1))
+        if y < maze.shape[1] - 1: neighbors.append((x, y + 1))
         return neighbors
 
     def astar(start, goal):
@@ -33,7 +32,7 @@ def adaptive_astar(maze, start, goal, tie_breaking='smaller_g'):
                 return reconstruct_path(came_from, current), expanded_cells, g_score
 
             for neighbor in get_neighbors(*current):
-                if maze[neighbor[1], neighbor[0]] == -1:
+                if maze[neighbor[0], neighbor[1]] == -1:
                     continue
 
                 tentative_g_score = g_score[current] + 1
@@ -76,18 +75,17 @@ def compare_tie_breaking(maze, start, goal):
 def visualize_maze(maze, path, title):
     plt.imshow(maze, cmap='gray')
     if path is not None:
-        print("HIHIHI")
         for (x, y) in path:
             plt.plot(y, x, 'ro', markersize=2)  # Plot path as red dots
     plt.title(title)
     plt.show()
 
 if __name__ == "__main__":
-    for i in range(50):
+    for i in range(5,6):
         filename = f"gridWorlds/gridworld_{i}.npy"
         maze = np.load(filename)
         start = (0, 0)
-        goal = (maze.shape[1] - 1, maze.shape[0] - 1)
+        goal = (maze.shape[0] - 1, maze.shape[1] - 1)
 
         (smaller_g_result, larger_g_result) = compare_tie_breaking(maze, start, goal)
 
